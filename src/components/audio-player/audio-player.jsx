dba3fcecd0cc97707e10c786782aef9daee167f5
Alexander Olshanskiy <sander2411ukr@gmail.com>
2020-03-05 20:45:34 +0200

    transfers state to HOCs

--- src/components/audio-player/audio-player.jsx
+++ src/components/audio-player/audio-player.jsx
@@ -1,56 +1,9 @@
-import React, {PureComponent, Fragment, createRef} from 'react';
+import React, {PureComponent, Fragment} from 'react';
 import PropTypes from 'prop-types';
 
 export default class AudioPlayer extends PureComponent {
-  constructor(props) {
-    super(props);
-    this.state = {
-      progress: 0,
-      isLoading: true,
-      isPlaying: this.props.isPlaying,
-    };
-    this._audioRef = createRef();
-  }
-
-  componentDidMount() {
-    const {src} = this.props;
-    const audio = this._audioRef.current;
-
-    audio.src = src;
-
-    audio.oncanplaythrough = () => this.setState({isLoading: false});
-
-    audio.onplay = () => this.setState({isPlaying: true});
-
-    audio.onpause = () => this.setState({isPlaying: false});
-
-    audio.ontimeupdate = () => this.setState({progress: audio.currentTime});
-  }
-
-  componentWillUnmount() {
-    const audio = this._audioRef.current;
-
-    audio.src = ``;
-    audio.oncanplaythrough = null;
-    audio.onplay = null;
-    audio.onpause = null;
-    audio.ontimeupdate = null;
-  }
-
-  componentDidUpdate() {
-    const audio = this._audioRef.current;
-
-    if (this.props.isPlaying) {
-      audio.play();
-      return;
-    }
-
-    audio.pause();
-  }
-
   render() {
-    const {isLoading, isPlaying} = this.state;
-    const {onPlayButtonClick} = this.props;
+    const {isLoading, isPlaying, onPlayButtonClick, children} = this.props;
 
     return (
       <Fragment>
@@ -58,13 +11,10 @@ export default class AudioPlayer extends PureComponent {
           className={`track__button track__button--${isPlaying ? `pause` : `play`}`}
           type="button"
           disabled={isLoading}
-          onClick={() => {
-            this.setState({isPlaying: !this.state.isPlaying});
-            onPlayButtonClick();
-          }}
+          onClick={() => onPlayButtonClick()}
         />
         <div className="track__status">
-          <audio ref={this._audioRef} />
+          {children}
         </div>
       </Fragment>
     );
@@ -72,7 +22,11 @@ export default class AudioPlayer extends PureComponent {
 }
 
 AudioPlayer.propTypes = {
+  isLoading: PropTypes.bool.isRequired,
   isPlaying: PropTypes.bool.isRequired,
   onPlayButtonClick: PropTypes.func.isRequired,
-  src: PropTypes.string.isRequired,
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node
+  ]).isRequired,
 };
--- src/components/genre-question-screen/genre-question-screen.jsx
+++ src/components/genre-question-screen/genre-question-screen.jsx
@@ -1,59 +1,33 @@
 import React, {PureComponent} from 'react';
 import PropTypes from 'prop-types';
+import GenreQuestionItem from '../genre-question-item/genre-question-item.jsx';
 import {GameType} from '../../const.js';
 
 export default class GenreQuestionScreen extends PureComponent {
-  constructor(props) {
-    super(props);
-    this.state = {
-      answers: [false, false, false, false],
-    };
-
-    this._handleCheckboxChange = this._handleCheckboxChange.bind(this);
-    this._handleFormSubmit = this._handleFormSubmit.bind(this);
-  }
-
-  _handleCheckboxChange(evt, i) {
-    const value = evt.target.checked;
-
-    this.setState({
-      answers: [...this.state.answers.slice(0, i), value, ...this.state.answers.slice(i + 1)],
-    });
-  }
-
-  _handleFormSubmit(evt) {
-    evt.preventDefault();
-
-    this.props.onAnswer(this.props.question, this.state.answers);
-  }
-
   render() {
-    const {renderPlayer} = this.props;
-    const {answers, genre} = this.props.question;
+    const {onAnswer, onChange, question, renderPlayer, userAnswers} = this.props;
+    const {answers, genre} = question;
+
 
     return (
       <section className="game__screen">
         <h2 className="game__title">Выберите {genre} треки</h2>
         <form
           className="game__tracks"
-          onSubmit={this._handleFormSubmit}
+          onSubmit={(evt) => {
+            evt.preventDefault();
+            onAnswer();
+          }}
         >
           {answers.map((answer, i) => (
-            <div key={`${i}-${answer.src}`} className="track">
-
-              {renderPlayer(answer.src, i)}
-
-              <div className="game__answer">
-                <input className="game__input visually-hidden" type="checkbox" name="answer" value={`answer-${i}`}
-                  id={`answer-${i}`}
-                  checked={this.state.answers[i]}
-                  onChange={(evt) => {
-                    this._handleCheckboxChange(evt, i);
-                  }}
-                />
-                <label className="game__check" htmlFor={`answer-${i}`}>Отметить</label>
-              </div>
-            </div>
+            <GenreQuestionItem
+              answer={answer}
+              id={i}
+              key={`${i}-${answer.src}`}
+              onChange={onChange}
+              renderPlayer={renderPlayer}
+              userAnswer={userAnswers[i]}
+            />
           ))}
 
           <button className="game__submit button" type="submit">Ответить</button>
@@ -65,6 +39,7 @@ export default class GenreQuestionScreen extends PureComponent {
 
 GenreQuestionScreen.propTypes = {
   onAnswer: PropTypes.func.isRequired,
+  onChange: PropTypes.func.isRequired,
   question: PropTypes.shape({
     answers: PropTypes.arrayOf(PropTypes.shape({
       src: PropTypes.string.isRequired,
@@ -74,5 +49,5 @@ GenreQuestionScreen.propTypes = {
     type: PropTypes.oneOf([GameType.ARTIST, GameType.GENRE]).isRequired,
   }).isRequired,
   renderPlayer: PropTypes.func.isRequired,
+  userAnswers: PropTypes.arrayOf(PropTypes.bool).isRequired,
 };
-
