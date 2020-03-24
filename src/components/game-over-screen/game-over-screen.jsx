f772df6f3b810d721497827d922910a7f647e357
Alexander Olshanskiy <sander2411ukr@gmail.com>
2020-03-24 12:39:31 +0200

    8.1 Implements routing with history

--- src/components/game-over-screen/game-over-screen.jsx
+++ src/components/game-over-screen/game-over-screen.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {Link} from 'react-router-dom';
+import {AppRoute} from '../../const.js';
 
 const GameOverScreen = ({onReplayButtonClick}) => {
   return (
@@ -9,13 +11,13 @@ const GameOverScreen = ({onReplayButtonClick}) => {
       </div>
       <h2 className="result__title">Какая жалость!</h2>
       <p className="result__total result__total--fail">У вас закончились все попытки. Ничего, повезёт в следующий раз!</p>
-      <button
+      <Link
         className="replay"
-        type="button"
+        to={AppRoute.ROOT}
         onClick={onReplayButtonClick}
       >
         Попробовать ещё раз
-      </button>
+      </Link>
     </section>
   );
 };
--- src/components/game-screen/game-screen.test.js
+++ src/components/game-screen/game-screen.test.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import {Router} from 'react-router-dom';
 import {GameScreen} from './game-screen.jsx';
 import {GameType} from '../../const.js';
+import history from '../../history.js';
 
 const children = <div className="children-component" />;
 
 describe(`GameScreen component render correctly`, () => {
   it(`with type GameType.ARTIST`, () => {
     const markup = renderer.create(
-        <GameScreen type={GameType.ARTIST} mistakes={3}>
-          {children}
-        </GameScreen>
+        <Router history={history}>
+          <GameScreen
+            type={GameType.ARTIST}
+            mistakes={3}
+            goToWelcome={() => {}}>
+
+            {children}
+          </GameScreen>
+        </Router>
     ).toJSON();
 
     expect(markup).toMatchSnapshot();
@@ -18,9 +26,15 @@ describe(`GameScreen component render correctly`, () => {
 
   it(`with type GameType.GENRE`, () => {
     const markup = renderer.create(
-        <GameScreen type={GameType.GENRE} mistakes={3}>
-          {children}
-        </GameScreen>
+        <Router history={history}>
+          <GameScreen
+            type={GameType.GENRE}
+            mistakes={3}
+            goToWelcome={() => {}}>
+
+            {children}
+          </GameScreen>
+        </Router>
     ).toJSON();
 
     expect(markup).toMatchSnapshot();
